9136488732dafa2f6bbe05ddeff528353683a610
succeedwer <succeedwer@126.com>
2015-11-24 20:02:00 +0800

    change2

--- gitTest/ViewController.m
+++ gitTest/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString * str;
+   
+    NSString * changeStr;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
